4dcd11d50c8507fe225aa1c64e2665a6575251cf
Kevin Wong <kevingywong@gmail.com>
2021-11-04 22:21:10 -0400

    refactored JavaScript file

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 let btn = document.getElementById("btn");
-let output = document.getElementById("output");
+let output = document.getElementById("output"); 
+let input = document.getElementById("userInput");
 
 let number = 8;
 
-btn.addEventListener("click", function guess(num) {
-    let input = document.getElementById("userInput").value;
-    if (isNaN(input)){
+function guess() {
+    let guess = input.value;
+    if (guess.length == 0 || isNaN(guess)){
         alert("Please give me a whole number!");
-    } if (input > number) {
+        output.innerHTML = "Enter a number below:";
+    } else if (guess > number) {
         output.innerHTML = "You guessed too high!";
-    } else if (input < number) {
+    } else if (guess < number) {
         output.innerHTML = "You guessed too low!";
     } else {
         output.innerHTML = "Congrats! You guessed the correct number!"}
-    });
+    }
+
+btn.addEventListener("click", guess);
+
+input.addEventListener("keyup", function enter(e) {
+    if (e.keyCode == 13) {
+        guess();
+    }
+})
